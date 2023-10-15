111c725baf9a05ef2eabd7dc23ab7f6b63a350a1
satishvatturu <93177666+satishvatturu@users.noreply.github.com>
2023-10-15 19:52:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function decimalToBinary(num) {
-	console.log(num.toString(2));  
+	return (num>>>0).toString(2); 
 }
+
+let num = prompt("Enter the value :-");
+let bin = decimalToBinary(num);
+console.log(bin)
 
 window.decimalToBinary = decimalToBinary;
